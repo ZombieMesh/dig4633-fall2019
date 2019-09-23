5335accd2c211ff8861c8cd6ea16a7471f36f698
ZombieMesh <33169504+ZombieMesh@users.noreply.github.com>
2019-09-23 18:07:54 -0400

    fixed bucket, added script for apple snap

---
+++ lab-vr/Assets/Handletrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Handletrigger : MonoBehaviour
+
+{
+    // Start is called before the first frame update
+    public void Handlesnap()
+    {
+        Debug.Log("snap");
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
